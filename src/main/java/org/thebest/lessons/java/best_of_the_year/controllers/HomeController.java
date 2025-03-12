9fa721478f5f9b18caef33de0c559a2726ef5f55
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-12 19:40:07 +0100

    Step 1

---
+++ src/main/java/org/thebest/lessons/java/best_of_the_year/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package org.thebest.lessons.java.best_of_the_year.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+        @GetMapping("/")
+        public String home (@RequestParam("param") String name, Model model) {
+            model.addAttribute("name", name);
+            return new String("index");
+        }
+        
+        
+}
